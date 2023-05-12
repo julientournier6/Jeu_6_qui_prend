92f534774cca29b39bf970607ac7f49bdc0a261b
annabsalin <salinannabelle@gmail.com>
2023-05-12 11:37:10 +0200

    ajout fonction carte, jeu et joueur

--- src/main/java/com/example/jeu6quiprend/Carte.java
+++ src/main/java/com/example/jeu6quiprend/Carte.java
@@ -1,4 +1,30 @@
 package com.example.jeu6quiprend;
 
 public class Carte {
+    private int numero;
+    private int valeur;
+
+    public Carte(int numero, int valeur) {
+        this.numero = numero;
+        this.valeur = valeur;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    public int comparer(Carte autreCarte) {
+        if (this.valeur < autreCarte.valeur) {
+            return -1;
+        } else if (this.valeur > autreCarte.valeur) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
+
--- src/main/java/com/example/jeu6quiprend/Jeu.java
+++ src/main/java/com/example/jeu6quiprend/Jeu.java
@@ -1,4 +1,57 @@
 package com.example.jeu6quiprend;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Jeu {
+    private ArrayList<Carte> cartes;
+    private ArrayList<Joueur> joueurs;
+    private ArrayList<Carte> tasDeCartes;
+
+    public Jeu(ArrayList<Joueur> joueurs) {
+        this.joueurs = joueurs;
+        this.cartes = new ArrayList<Carte>();
+        this.tasDeCartes = new ArrayList<Carte>();
+
+        // Initialisation du jeu de cartes
+        for (int i = 1; i <= 104; i++) {
+            int valeur = i % 10 == 0 ? 10 : i % 10;
+            this.cartes.add(new Carte(i, valeur));
+        }
+        Collections.shuffle(this.cartes);
+
+        // Distribution des cartes aux joueurs
+        for (Joueur joueur : this.joueurs) {
+            for (int i = 0; i < 10; i++) {
+                joueur.ajouterCarte(this.cartes.remove(0));
+            }
+        }
+
+        // Initialisation du tas de cartes
+        for (int i = 0; i < 4; i++) {
+            this.tasDeCartes.add(this.cartes.remove(0));
+        }
+    }
+
+    public ArrayList<Carte> getTasDeCartes() {
+        return tasDeCartes;
+    }
+
+    public void ajouterCarteAuTas(Carte carte) {
+        tasDeCartes.add(carte);
+    }
+
+    public void retirerCarteDuTas(Carte carte) {
+        tasDeCartes.remove(carte);
+    }
+
+    public void jouerTour() {
+        // TODO: implémenter le tour de jeu
+    }
+
+    public void afficherScores() {
+        for (Joueur joueur : joueurs) {
+            System.out.println(joueur.getNom() + " : " + joueur.getPoints() + " points");
+        }
+    }
 }
