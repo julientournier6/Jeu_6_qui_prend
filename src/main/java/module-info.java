03809710cc3c3bc872cdf7c3ae9c669fabe9140d
julien tournier <56165771+julientournier6@users.noreply.github.com>
2023-05-12 09:04:42 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.jeu6quiprend {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens com.example.jeu6quiprend to javafx.fxml;
+    exports com.example.jeu6quiprend;
+}
